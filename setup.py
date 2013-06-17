74f7797c44601befc5cb547c16f468dd7f560207
Manuel Barkhau <mbarkhau@gmail.com>
2013-06-17 03:48:00 +0200

    fixes for clean install from pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from distutils.core import setup
+from os.path import join, dirname
+
+
+def read(fname):
+    with open(join(dirname(__file__), fname), 'r') as f:
+        return f.read()
+
 
 setup(
     name='python-propeller',
-    version='0.2.1',
+    version='0.2.3',
     description='Pretty progress and load indicators',
     author='Thomas Einsporn, Manuel Barkhau',
     author_email='mb@nexttuesday.de, mb@nexttuesday.de',
-    long_description=open("README.md", 'r').read(),
+    long_description=read("README.md"),
     license="BSD",
     url="https://github.com/mbarkhau/python-propeller",
     download_url="https://github.org/mbarkhau/python-propeller/",
